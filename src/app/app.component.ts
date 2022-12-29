576ccad74b85d7d9d498d4965d2cadaf2fcd017d
Omar125Radwan <oradwan007@gmail.com>
2022-12-29 19:44:02 +0200

    Angular Localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-localization';
+  langCode: string = 'ar';
+  isLangLoaded = false;
+  languages: any[] = [];
+  currentLangCode = 'ar';
+  lang: any;
+  constructor(private translateService: TranslateService) {
+  }
+  ngOnInit(): void {
+    this.initializeLanguages();
+    this.onLangSelected(this.currentLangCode);
+  }
+  initializeLanguages(): void {
+    this.languages = [
+      {code: 'en', dir: 'ltr', name: this.translateService.instant('languages.english')},
+      {code: 'ar', dir: 'rtl', name: this.translateService.instant('languages.arabic')},
+    ]
+  }
+  onLangSelected(langCode: string): void {
+    this.currentLangCode = langCode;
+    this.lang = this.languages.find(l => l.code === this.currentLangCode);
+    this.translateService.setDefaultLang(this.lang.code);
+    this.translateService.use(this.lang.code);
+  }
+
 }
+
+
+/*//! camelCase: Vars, functions, object, properties,
+  //! PascalCase: Classes, Interfaces, Enums,
+  //! snake_case: const, translations, key
+  //! kabab-case: css, files, folders name, website-logo*/
+/* languages: any[] = [
+    {code: 'en', dir: 'ltr', name: 'english'},
+    {code: 'ar', dir: 'rtl', name: 'arabic'},
+  ] */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+function httpLoaderFactoryFn(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, "./assets/i18n/", ".json");
+}
+function httpApiLoaderFactoryFn(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient, "https://www.google.com/api/teanslation/", "");
+}
+
+let directoryLoader = {
+  provide: TranslateLoader,
+  useFactory: httpLoaderFactoryFn,
+  deps: [HttpClient],
+}
+
+let apiLoader = {
+  provide: TranslateLoader,
+  useFactory: httpApiLoaderFactoryFn,
+  deps: [HttpClient],
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +32,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot(
+      {
+        loader: directoryLoader,
+      }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
